d9ece70a20429633c7e83fe77e100d6f656c6c08
cynthiaoduol <cynthiaobu940@gmail.com>
2019-11-08 14:42:42 +0300

    create and activate a virtual environment

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringaschool/Documents/moringa-school-projects/Personal-gallery/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
