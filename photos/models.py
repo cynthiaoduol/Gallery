eb33755f1e05644b8aa6e18983060f2115b2ac44
cynthiaoduol <cynthiaobu940@gmail.com>
2019-11-09 20:10:03 +0300

    create category and location models

--- photos/models.py
+++ photos/models.py
@@ -5,6 +5,9 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.TextField()
     image = models.ImageField(upload_to = 'images/',default='')
+    # image_location = models.ForeignKey(Location)
+    # image_category = models.ForeignKey(Category)
+
 
     def save_image(self):
         self.save()
@@ -13,4 +16,21 @@ class Image(models.Model):
         return self.image_name
 
 
+class Category(models.Model):
+    category_name = models.CharField(max_length =30)
 
+    def save_category(self):
+        self.save()
+
+    def __str__(self):
+        return self.category_name
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        self.save()
+
+    def __str__(self):
+        return self.location_name
