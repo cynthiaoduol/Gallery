1b6f1b19530ee2a3f190612040b4a5e71b1f4c8b
cynthiaoduol <cynthiaobu940@gmail.com>
2019-11-09 19:17:37 +0300

    create a n image model with an image name,image and description

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    image = models.ImageField(upload_to = 'images/',default='')
+
+    def save_image(self):
+        self.save()
+
+    def __str__(self):
+        return self.image_name
+
+
+
